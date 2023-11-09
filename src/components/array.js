e252a78acebf752c188191e48f666860c2b3c72d
coderfauzan <mahzaibfauzan@gmail.com>
2023-11-09 20:39:23 +0530

    first commit

---
+++ src/components/array.js
@@ -0,0 +1,23 @@
+// Filename - components/array.js
+
+// Javascript object named array 
+// with 3 key-values
+const array = [
+	{
+		id: "1",
+		Name: "abc",
+		Age: "23",
+	},
+	{
+		id: "2",
+		Name: "fgh",
+		Age: "22",
+	},
+	{
+		id: "3",
+		Name: "xyz",
+		Age: "23",
+	},
+];
+
+export default array;
